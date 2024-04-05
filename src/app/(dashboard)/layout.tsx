7520fa649efb484cf45754ef8289f4708c8f45f8
unknown <mojahidislammishat@gmail.com>
2024-04-05 20:32:55 +0600

    check routing problem

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Sidebar from "../(dashboard)/components/Sidebar/Sidebar";
+import Sidebar from "./components/Sidebar/page";
+
 
 interface DashboardLayoutProps {
   children: ReactNode;
@@ -11,7 +12,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
     <div className="w-full lg:w-[1440px] mx-auto bg-[#F6F6F6]">
       <Navbar />
       <div className="flex mt-8 mx-[50px] gap-12">
-        <Sidebar />
+      <Sidebar/>
         {children}
       </div>
     </div>
